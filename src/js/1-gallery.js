3bb22271be060b1a3f226aa57dc9911165e7f1da
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2024-01-23 20:42:46 +0100

    test

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,8 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+iziToast.show('gallery');
 const images = [
   {
     preview:
